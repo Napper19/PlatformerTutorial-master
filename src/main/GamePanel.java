25ada1ced9d7ab7f2cb0bd1bcaa37ce9b3169cb8
Napper19 <marionappo19@gmail.com>
2022-07-06 20:44:21 +0200

    Fixed try catch with finally in private void importImg()

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -37,6 +37,12 @@ public class GamePanel extends JPanel {
             img = ImageIO.read(is);
         }catch (IOException e){
             e.printStackTrace();
+        }finally {
+            try{
+                is.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
